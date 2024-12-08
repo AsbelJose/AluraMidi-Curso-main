2f5acc1f4ee538b4fd030823ba6af7a2c29d9246
AsbelJose <asbeljose@hotmail.com>
2024-12-08 16:40:11 -0500

    Buscar un elemento HTML y usarlo dentro de JS

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
-// probar que funciona la conexion entre HTML y JS
-alert('Hola mundo');
+function playSonidoPom () {
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
